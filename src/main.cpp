35f9451443dd9d89510c64c860dec3f9d8b6f34d
die-ene <enecoin@gmail.com>
2016-03-09 10:33:12 +0000

    Pos Reward Fix
    Fixed Pos percent for the 120% a year reward. My mistake sorry guy's

--- src/main.cpp
+++ src/main.cpp
@@ -982,10 +982,14 @@ int64_t GetProofOfWorkReward(int64_t nFees)
 // miner's coin stake reward based on coin age spent (coin-days)
 int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
 {
-    int64_t nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8); // 120%
-    if(pindexBest-> nHeight > 100000)
+    int64_t nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8);
+    if(pindexBest-> nHeight < 100000)
     {
-        nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8) / 10; // 12%
+        nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8) * 10;
+    }
+    else if(pindexBest-> nHeight > 100000)
+    {
+        nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8); // 12%
     }
 
     return nSubsidy + nFees;
